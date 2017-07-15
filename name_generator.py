db7a89cbf13d44cfda8319f7f20b198c0b461859
ja3067 <ja3067@columbia.edu>
2017-07-15 14:10:59 -0400

    Update name_generator.py

--- name_generator.py
+++ name_generator.py
@@ -2,7 +2,7 @@ import names
 import random
 import os
 
-os.chdir('/Users/JAustin/Documents/C++')
+os.chdir('/Users/.../Documents/C++')
 with open('example_short_space.txt', 'w+') as file:
     for i in range(100):
         file.write('%s %d %d %d %d %d %d \n' %(names.get_first_name(), 100*random.random(), 100*random.random(), 100*random.random(), 100*random.random(), 100*random.random(), 100*random.random()))
