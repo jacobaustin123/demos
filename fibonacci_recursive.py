3e3156f3f03ecbf8bb297e735a238f9ebf441958
ja3067 <ja3067@columbia.edu>
2017-07-20 12:33:37 -0400

    Create fibonacci_recursive.py

---
+++ fibonacci_recursive.py
@@ -0,0 +1,9 @@
+def fib(n, computed = {0: 0, 1: 1)):
+    if n not in computed:
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    return computed[n]
+    
+print(fib(50))
+  
+  
+  
